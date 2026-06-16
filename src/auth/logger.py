d02342e93b7a7da799cd3e36927044a31d063a81
Krish <krishmajhi9003@gmail.com>
2026-06-16 21:06:34 +0530

    Configure logger to use INFO level
    Set logger level to INFO and configure logging format.

--- src/auth/logger.py
+++ src/auth/logger.py
@@ -2,10 +2,9 @@ import logging
 
 logger = logging.getLogger("auth_logger")
 
+logger.setLevel(logging.INFO)
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
-
-logging.info("TEST LOG")
-logger.warning("TEST WARNING")
